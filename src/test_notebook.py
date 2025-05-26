197f086f8cdaafe83540d27efdd060dc99d83ca1
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2025-05-26 16:59:00 +0200

    start adding interpolation function

--- src/test_notebook.py
+++ src/test_notebook.py
@@ -46,6 +46,46 @@ def _(aw, np, plt):
 
 
 @app.cell
+def _(aw, np, plt):
+    def interpolate(x_in, y_in, x_out, neighbors, extrapolate: bool = False):
+        sort_order = np.argsort(x_in)
+        x_in_sorted = x_in[sort_order]
+        y_in_sorted = y_in[sort_order]
+        y_out = np.zeros(x_out.shape)
+
+        def interpolate_point(x):
+            location = np.searchsorted(x_in_sorted, x_out[_i], side='left')
+
+            if (((location == 0) and x != x_in_sorted[0]) or (location >= len(x_in_sorted))):
+                #points outside the range of x_in (extrapolation)
+                if extrapolate:
+                    return 3.0
+                else:
+                    return 0.0
+        
+            elif x == x_in_sorted[location]:
+                #case if x is exactly in the x_in array
+                #check if the next point also matches, careful not to go outside the array
+                return 1.0
+            
+            else:
+                #normal interior point
+                return 2.0
+                #x is not in the array
+
+        for _i in range(len(x_out)):
+            y_out[_i] = interpolate_point(x_out[_i])
+
+        return y_out
+
+    x = np.sqrt(np.linspace(0.0,10.0,16))
+    y = x**4
+    plt.plot(interpolate(x,y,np.array([0, 0.5, 1.0, 2.0, 4]), 1),'x')
+    aw.plot.showmo()
+    return
+
+
+@app.cell
 def _():
     return
 
