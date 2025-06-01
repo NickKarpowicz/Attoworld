d8aa80cf801983a20cf50ecf3ec689620ed10b8a
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2025-06-01 22:28:04 +0200

    add find_maximum_location() to Rust lib

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use pyo3::prelude::*;
 fn attoworld_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(rust_hello, m)?)?;
     m.add_function(wrap_pyfunction!(fornberg_stencil_wrapper, m)?)?;
+    m.add_function(wrap_pyfunction!(find_maximum_location_wrapper, m)?)?;
     Ok(())
 }
 
@@ -16,9 +17,21 @@ fn rust_hello() -> PyResult<()> {
     Ok(())
 }
 
+/// Find the location and value of the maximum of a smooth, uniformly sampled signal, interpolating to find the sub-pixel location
+///
+/// Args:
+///     y (np.ndarray): The signal whose maximum should be located
+///     neighbors (int): the number of neighboring points to consider in the optimization (default 3)
+///
+/// Returns:
+///     (float, float): location, interpolated maximum
 #[pyfunction]
-#[pyo3(name = "fornberg_stencil")]
-#[pyo3(signature = (order, positions, position_out = 0.0, /))]
+#[pyo3(name = "find_maximum_location")]
+#[pyo3(signature = (y, neighbors = 3, /))]
+fn find_maximum_location_wrapper(y: Vec<f64>, neighbors: i64) -> (f64, f64) {
+    find_maximum_location(&y, neighbors)
+}
+
 /// Generate a finite difference stencil using the algorithm described by B. Fornberg
 /// in Mathematics of Computation 51, 699-706 (1988).
 ///
@@ -34,6 +47,9 @@ fn rust_hello() -> PyResult<()> {
 ///     >>> stencil = fornberg_stencil(1, [-1,0,1])
 ///     >>> print(stencil)
 ///     [-0.5 0. 0.5]
+#[pyfunction]
+#[pyo3(name = "fornberg_stencil")]
+#[pyo3(signature = (order, positions, position_out = 0.0, /))]
 fn fornberg_stencil_wrapper(
     order: usize,
     positions: Vec<f64>,
@@ -47,12 +63,12 @@ fn fornberg_stencil(order: usize, positions: &[f64], position_out: f64) -> Vec<f
     let n_pos = positions.len();
     let mut delta_current = vec![vec![0.0; order + 1]; n_pos];
     let mut delta_previous = vec![vec![0.0; order + 1]; n_pos];
-    delta_previous[0][0] = 1.0;
+    delta_current[0][0] = 1.0;
 
     let mut c1 = 1.0;
     for n in 1..n_pos {
+        std::mem::swap(&mut delta_previous, &mut delta_current);
         let mut c2 = 1.0;
-
         for v in 0..n {
             let c3 = positions[n] - positions[v];
             c2 *= c3;
@@ -87,10 +103,68 @@ fn fornberg_stencil(order: usize, positions: &[f64], position_out: f64) -> Vec<f
         }
 
         c1 = c2;
-        if n < (n_pos - 1) {
-            std::mem::swap(&mut delta_previous, &mut delta_current);
-        }
     }
 
     (0..n_pos).map(|v| delta_current[v][order]).collect()
+}
+
+fn find_maximum_location(y: &[f64], neighbors: i64) -> (f64, f64) {
+    let max_index: i64 = y
+        .iter()
+        .enumerate()
+        .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+        .unwrap()
+        .0 as i64;
+
+    let start_index: usize =
+        if (max_index - neighbors) >= 0 && (max_index + neighbors) < y.len() as i64 {
+            if y[(max_index + 1) as usize] > y[(max_index - 1) as usize] {
+                (max_index - neighbors + 1) as usize
+            } else {
+                (max_index - neighbors) as usize
+            }
+        } else if (max_index - neighbors) < 0 {
+            0usize
+        } else {
+            y.len() - 2 * neighbors as usize - 1usize
+        };
+
+    let stencil_positions: Vec<f64> = (start_index..(start_index + (2 * neighbors) as usize))
+        .map(|x| x as f64)
+        .collect();
+    let mut derivatives: Vec<f64> = vec![0.0; (2 * neighbors) as usize + 1usize];
+    for n in 0usize..=((2 * neighbors) as usize) {
+        let stencil = fornberg_stencil(
+            1usize,
+            &stencil_positions,
+            (max_index - 1) as f64 + (n as f64) / (neighbors as f64),
+        );
+        derivatives[n] = stencil
+            .iter()
+            .zip(y[start_index..(start_index + 2 * neighbors as usize)].iter())
+            .map(|(x, y)| x * y)
+            .sum();
+    }
+    //println!("derivatives {:?}", derivatives);
+
+    let zero_xing_positions: Vec<f64> = (0..=(2 * neighbors))
+        .map(|x| (max_index - 1) as f64 + (x as f64) / (neighbors as f64))
+        .collect();
+    let zero_xing_stencil = fornberg_stencil(0, &derivatives, 0.0);
+
+    let location: f64 = zero_xing_stencil
+        .iter()
+        .zip(zero_xing_positions.iter())
+        .map(|(x, y)| x * y)
+        .sum();
+
+    let interpolation_stencil = fornberg_stencil(0usize, &stencil_positions, location);
+
+    let interpolated_max = interpolation_stencil
+        .iter()
+        .zip(y[start_index..(start_index + 2 * neighbors as usize)].iter())
+        .map(|(x, y)| x * y)
+        .sum();
+
+    (location, interpolated_max)
 }
