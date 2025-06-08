8d5db878b0c8d834dec6e0817d9ece96dbf75c3b
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2025-06-08 22:05:03 +0200

    better match logic in set_style

--- python/attoworld/plot/plot.py
+++ python/attoworld/plot/plot.py
@@ -22,25 +22,6 @@ def set_style(mode: str = 'light'):
                         ```nick_dark```: dark mode that matches Nick's slides
     """
     match mode:
-        case 'light':
-
-            plt.rcParams.update({'font.sans-serif': ['Helvetica', 'Arial', 'Verdana', 'DejaVu Sans', 'Liberation Sans', 'Bitstream Vera Sans', 'sans-serif']})
-            plt.rcParams.update({'font.family': 'sans-serif'})
-            #colorblind-friendly color cycle from https://gist.github.com/thriveth/8560036
-            plt.rcParams.update({'axes.prop_cycle': cycler(color=['#377eb8', '#ff7f00', '#4daf4a', '#f781bf', '#a65628', '#984ea3', '#999999', '#e41a1c', '#dede00'])})
-            plt.rcParams.update({'figure.facecolor': 'white'})
-            plt.rcParams.update({'figure.edgecolor':'white'})
-            plt.rcParams.update({'savefig.facecolor': 'white'})
-            plt.rcParams.update({'savefig.edgecolor': 'white'})
-            plt.rcParams.update({'axes.facecolor': 'white'})
-            plt.rcParams.update({'text.color': 'black'})
-            plt.rcParams.update({'axes.edgecolor': 'black'})
-            plt.rcParams.update({'axes.labelcolor': 'black'})
-            plt.rcParams.update({'xtick.color': 'black'})
-            plt.rcParams.update({'ytick.color': 'black'})
-            plt.rcParams.update({'grid.color': 'black'})
-            plt.rcParams.update({'lines.color': 'black'})
-
         case 'nick_dark':
             plt.rcParams.update({'font.sans-serif': ['Helvetica', 'Arial', 'Verdana', 'DejaVu Sans', 'Liberation Sans', 'Bitstream Vera Sans', 'sans-serif']})
             plt.rcParams.update({'font.family': 'sans-serif'})
@@ -57,3 +38,22 @@ def set_style(mode: str = 'light'):
             plt.rcParams.update({'ytick.color': 'white'})
             plt.rcParams.update({'grid.color': 'white'})
             plt.rcParams.update({'lines.color': 'white'})
+        # Light case is combined with _, which will capture anything else that didn't match.
+        # It must be the last case, for that reason.
+        case 'light' | _:
+            plt.rcParams.update({'font.sans-serif': ['Helvetica', 'Arial', 'Verdana', 'DejaVu Sans', 'Liberation Sans', 'Bitstream Vera Sans', 'sans-serif']})
+            plt.rcParams.update({'font.family': 'sans-serif'})
+            #colorblind-friendly color cycle from https://gist.github.com/thriveth/8560036
+            plt.rcParams.update({'axes.prop_cycle': cycler(color=['#377eb8', '#ff7f00', '#4daf4a', '#f781bf', '#a65628', '#984ea3', '#999999', '#e41a1c', '#dede00'])})
+            plt.rcParams.update({'figure.facecolor': 'white'})
+            plt.rcParams.update({'figure.edgecolor':'white'})
+            plt.rcParams.update({'savefig.facecolor': 'white'})
+            plt.rcParams.update({'savefig.edgecolor': 'white'})
+            plt.rcParams.update({'axes.facecolor': 'white'})
+            plt.rcParams.update({'text.color': 'black'})
+            plt.rcParams.update({'axes.edgecolor': 'black'})
+            plt.rcParams.update({'axes.labelcolor': 'black'})
+            plt.rcParams.update({'xtick.color': 'black'})
+            plt.rcParams.update({'ytick.color': 'black'})
+            plt.rcParams.update({'grid.color': 'black'})
+            plt.rcParams.update({'lines.color': 'black'})
