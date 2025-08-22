79bcc320179eecc34bfbf18f5129bfc0db5176b5
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2025-08-22 15:09:28 +0200

    Add power method SHG-FROG iteration function (not used at the moment since the speed bottleneck isn't in the SVD)

--- python/attoworld/wave/frog.py
+++ python/attoworld/wave/frog.py
@@ -114,15 +114,38 @@ def apply_iteration(Et, Gt, meas_sqrt):
     gate = v[0, :].squeeze()
     return field, gate
 
+def apply_power_method_iteration(Et, Gt, meas_sqrt, power_method_iterations:int=2):
+    """Apply an iteration of the generalized projections SHG-FROG.
 
-def calculate_g_error(measurement, pulse):
+    Args:
+        Et: field
+        Gt: gate
+        meas_sqrt: the measurement, sqrt-ed and fftshift, axes=0
+
+    Returns:
+        np.ndarray, np.ndarray: field, gate
+
+    """
+    new_sg = generate_shg_spectrogram(Et, Gt)
+    new_sg = meas_sqrt * np.exp(1j * np.angle(new_sg))
+    new_sg = np.fft.ifft(new_sg, axis=0)
+    for _i in range(len(Et)):
+        new_sg[_i, :] = blank_roll(new_sg[_i, :], _i - +int(Et.shape[0] / 2))
+
+    field = np.array(Et)
+    for _ in range(power_method_iterations):
+        field = np.dot(new_sg, field)
+        field /= np.linalg.norm(field)
+
+    return field, field
+
+
+def calculate_g_error(measurement_normalized, pulse):
     """Calculate G' error helper function."""
-    meas_squared = measurement**2
-    meas_squared /= np.linalg.norm(meas_squared)
-    reconstructed = np.abs(generate_shg_spectrogram(pulse, pulse)) ** 2
-    reconstructed /= np.linalg.norm(reconstructed)
+    recon_normalized = np.abs(generate_shg_spectrogram(pulse, pulse)) ** 2
+    recon_normalized /= np.linalg.norm(recon_normalized)
     return np.sqrt(
-        np.sum((meas_squared[:] - reconstructed[:]) ** 2) / np.sum(meas_squared[:] ** 2)
+        np.sum((measurement_normalized[:] - recon_normalized[:]) ** 2) / np.sum(measurement_normalized[:] ** 2)
     )
 
 
@@ -140,6 +163,8 @@ def reconstruct_shg_frog_core(
         np.ndarray: the reconstructed field
 
     """
+    measurement_norm = measurement_sg_sqrt**2
+    measurement_norm = measurement_norm/np.linalg.norm(measurement_norm)
     if guess is None:
         guess_pulse = np.random.randn(
             measurement_sg_sqrt.shape[0]
@@ -149,12 +174,12 @@ def reconstruct_shg_frog_core(
         gate = guess
     best = shift_to_zero_and_normalize(guess + gate)
     current = best
-    best_error = calculate_g_error(measurement_sg_sqrt, best)
+    best_error = calculate_g_error(measurement_norm, best)
     for _i in range(max_iterations):
         guess, gate = apply_iteration(current, current, measurement_sg_sqrt)
         current = shift_to_zero_and_normalize(guess + gate)
         current = fix_aliasing(current)
-        current_error = calculate_g_error(measurement_sg_sqrt, current)
+        current_error = calculate_g_error(measurement_norm, current)
         if current_error < best_error:
             best_error = current_error
             best = current
@@ -197,13 +222,15 @@ def reconstruct_shg_frog(
         np.sqrt(measurement.data - np.min(measurement.data[:])), axes=0
     )
     sqrt_sg /= np.max(sqrt_sg)
+    measurement_norm = sqrt_sg**2
+    measurement_norm = measurement_norm/np.linalg.norm(measurement_norm)
     results = np.zeros((sqrt_sg.shape[0], repeats), dtype=np.complex128)
     errors = np.zeros(repeats, dtype=float)
     for _i in range(repeats):
         results[:, _i] = reconstruct_shg_frog_core(
             sqrt_sg, max_iterations=test_iterations
         )
-        errors[_i] = calculate_g_error(sqrt_sg, results[:, _i])
+        errors[_i] = calculate_g_error(measurement_norm, results[:, _i])
     min_error_index = np.argmin(errors)
     result = reconstruct_shg_frog_core(
         sqrt_sg, guess=results[:, min_error_index], max_iterations=polish_iterations
